ad90dfd846f1f4a4421e857fceca12840fdbaed2
Alhousseny Bangoura <alhoussenyba05@gmail.com>
2026-01-13 02:43:01 +0000

    succes

--- index.js
+++ index.js
@@ -1,81 +1,47 @@
 btn=document.querySelectorAll("button");
 
-let audios = ["sounds/crash.mp3", "sounds/kick-bass.mp3", "sounds/snare.mp3", "sounds/tom-1.mp3", "sounds/tom-2.mp3", "sounds/tom-3.mp3", "sounds/tom-4.mp3"];
+let audios = ["sounds/tom-3.mp3","sounds/tom-1.mp3","sounds/crash.mp3", "sounds/kick-bass.mp3", "sounds/snare.mp3","sounds/tom-2.mp3", "sounds/tom-4.mp3"];
+let joue= new Audio("sounds/Dynasty__Official_Audio____MIIA(360p).mp3");
 
 for(let n=0; n<btn.length; n++)
 {
     btn[n].addEventListener("click", function()
     {
-        playsoudint(this.innerHTML);
+        playsoundClick(this.innerHTML);
+        boutonAnimation(this.innerHTML);
+        console.log(this.innerHTML);
     });
 }
 
-document.addEventListener("keydown", function(event){
-    playsoud(event.key);
-})
+document.addEventListener("keypress", function(event){
+    playsoundOld(event.key); 
+    console.log(event.key);
+    boutonAnimation(event.key); 
+});
 
-function playsoudint(key){
+function playsoundClick(key){
     switch(key){
-    case "space":
-        var audio = new Audio(audios[0]);
-        audio.play();
-        break;
-
-    case "a":
-        var audio = new Audio(audios[1]);
-        audio.play();
-        break;
-
-    case "s":
-        var audio = new Audio(audios[2]);
-        audio.play();
-        break;
-
     case "d":
-        var audio = new Audio(audios[3]);
-        audio.play();
-        break;
-
-    case "j":
-        var audio = new Audio(audios[4]);
-        audio.play();
-        break;  
-
-    case "k":
-        var audio = new Audio(audios[5]);
-        audio.play();
-        break;
-
-    case "l":
-        var audio = new Audio(audios[6]);
-        audio.play();
-        break;
-    }
-}
-
-function playsoud(key){
-    switch(key){
-    case " ":
         var audio = new Audio(audios[0]);
-        audio.play();
-        break;
-
-    case "l":
-        var audio = new Audio(audios[1]);
-        audio.play();
-        break;
-
-    case "s":
-        var audio = new Audio(audios[2]);
-        audio.play();
-        break;
-
-    case "d":
-        var audio = new Audio(audios[3]);
         audio.play();
         break;
 
     case "f":
+        var audio = new Audio(audios[1]);
+        audio.play();
+        break;
+
+    case "l":
+        var audio = new Audio(audios[2]);
+        audio.play();
+        break;
+
+    case "s":
+        var audio = new Audio(audios[3]);
+        audio.play();
+        break;
+
+    case "h":
         var audio = new Audio(audios[4]);
         audio.play();
         break;  
@@ -90,4 +56,51 @@ function playsoud(key){
         audio.play();
         break;
     }
+}
+
+function playsoundOld(key){
+    switch(key){
+    case "d":
+        var audio = new Audio(audios[0]);
+        audio.play();
+        break;
+
+    case "f":
+        var audio = new Audio(audios[1]);
+        audio.play();
+        break;
+
+    case "l":
+        var audio = new Audio(audios[2]);
+        audio.play();
+        break;
+
+    case "s":
+        var audio = new Audio(audios[3]);
+        audio.play();
+        break;
+
+    case "h":
+        var audio = new Audio(audios[4]);
+        audio.play();
+        break;  
+
+    case "j":
+        var audio = new Audio(audios[5]);
+        audio.play();
+        break;
+
+    case "k":
+        var audio = new Audio(audios[6]);
+        audio.play();
+        break;
+    }
+}
+
+function boutonAnimation(currentKey){
+    let activebtn=document.querySelector("."+currentKey);
+    activebtn.classList.add("pressed");
+    setTimeout(function(){
+        activebtn.classList.remove("pressed");
+    },200);
 }
